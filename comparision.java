e655b1618a371e5518cee4458cafbc019b7fef83
varsh-mh <121218612+varsh-mh@users.noreply.github.com>
2022-12-22 13:29:48 +0530

    Add files via upload

---
+++ comparision.java
@@ -0,0 +1,15 @@
+class comparision
+{
+	public static void main(String[] args) 
+	{
+		int a1=10,b1=20; 
+		System.out.println(a1>b1);
+		System.out.println(a1<b1);
+		System.out.println(a1<=b1);
+		System.out.println(a1>=b1);
+		System.out.println(a1==b1);
+		System.out.println(a1!=b1);
+		System.out.println(10>20);
+		
+	}
+}
